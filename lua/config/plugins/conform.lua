a82b80387f3e504df9a0b1ce8ff47a0500eca8f0
Bryan Brauchler <bbrauchl@umich.edu>
2024-03-19 07:46:27 -0400

    removing default formating settings within conform (to be done later)

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -13,7 +13,7 @@ local conform_config = { -- Autoformat
       }
     end,
     formatters_by_ft = {
-      lua = { 'stylua' },
+      -- lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
