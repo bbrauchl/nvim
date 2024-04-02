6e48326ad95c6c8ee5078216397342def63893ca
Bryan Brauchler <bbrauchl@umich.edu>
2024-04-02 00:02:45 -0400

    moving keymappings into centralized mappings file

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -176,6 +176,76 @@ M.plugin_mappings = {
     },
   },
 
+  -- [[ Configuration for the nvim-lspconfig Plugin ]]
+  nvim_lspconfig = {
+    general = {
+      n = {
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+        ['gd'] = {
+          function() require('telescope.builtin').lsp_definitions() end,
+          'LSP: [G]oto [D]efinition',
+        },
+        -- Find references for the word under your cursor.
+        ['gr'] = {
+          function() require('telescope.builtin').lsp_references() end,
+          '[LSP: G]oto [R]eferences',
+        },
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
+        ['gI'] = {
+          function() require('telescope.builtin').lsp_implementations() end,
+          'LSP: [G]oto [I]mplementation',
+        },
+        -- Jump to the type of the word under your cursor.
+        --  Useful when you're not sure what type a variable is and you want to see
+        --  the definition of its *type*, not where it was *defined*.
+        ['<leader>D'] = {
+          function() require('telescope.builtin').lsp_type_definitions() end,
+          'LSP: Type [D]efinition',
+        },
+        -- Fuzzy find all the symbols in your current document.
+        --  Symbols are things like variables, functions, types, etc.
+        ['<leader>ds'] = {
+          function() require('telescope.builtin').lsp_document_symbols() end,
+          'LSP: [D]ocument [S]ymbols',
+        },
+        -- Fuzzy find all the symbols in your current workspace.
+        --  Similar to document symbols, except searches over your entire project.
+        ['<leader>ws'] = {
+          function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end,
+          'LSP: [W]orkspace [S]ymbols',
+        },
+        -- Rename the variable under your cursor.
+        --  Most Language Servers support renaming across files, etc.
+        ['<leader>rn'] = {
+          function() vim.lsp.buf.rename() end,
+          'LSP: [R]e[n]ame',
+        },
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or a suggestion from your LSP for this to activate.
+        ['<leader>ca'] = {
+          function() vim.lsp.buf.code_action() end,
+          'LSP: [C]ode [A]ction',
+        },
+        -- Opens a popup that displays documentation about the word under your cursor
+        --  See `:help K` for why this keymap.
+        ['K'] = {
+          function() vim.lsp.buf.hover() end,
+          'LSP: Hover Documentation',
+        },
+        -- WARN: This is not Goto Definition, this is Goto Declaration.
+        --  For example, in C this would take you to the header.
+        ['gD'] = {
+          function() vim.lsp.buf.declaration() end,
+          'LSP: [G]oto [D]eclaration',
+        },
+      },
+    },
+  },
+
+
   -- [[ Configuration for Telescope Plugin ]]
   telescope = {
     general = {
