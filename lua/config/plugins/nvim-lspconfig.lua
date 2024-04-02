9f9f088f33da8409df19a94e0b314a421db63b1e
Bryan Brauchler <bbrauchl@umich.edu>
2024-04-01 23:29:22 -0400

    Adding configuration for additional LSPs

--- lua/config/plugins/nvim-lspconfig.lua
+++ lua/config/plugins/nvim-lspconfig.lua
@@ -134,10 +134,19 @@ local nvim_lspconfig_config = { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
+      arduino_language_server = {},
+      asm_lsp = {},
+      clangd = {},
+      cmake = {},
       -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
+      pyright = {},
+      rust_analyzer = {},
+      jsonls = {},
+      bashls = {},
+      dockerls = {},
+      docker_compose_language_service = {},
+      hdl_checker = {},
+      yamlls = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -175,6 +184,8 @@ local nvim_lspconfig_config = { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
+      'clang-format',
+      'asmfmt',
       'stylua', -- Used to format Lua code
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
